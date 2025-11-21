529f66561553ca4a7f86cd41107986f8d9e3bc84
Niroshan <niroshan9812@gmail.com>
2025-11-21 23:42:28 +0530

    Initial Commit

---
+++ IntelligentStP/src/main/java/com/intelligent/intelligentstp/IntelligentStPApplication.java
@@ -0,0 +1,13 @@
+package com.intelligent.intelligentstp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentStPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentStPApplication.class, args);
+    }
+
+}
